df8c21cb02e1b566f8a8ead60d7b536765492421
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-03-09 20:30:38 +0000

    Menu prompt questions added

--- index.js
+++ index.js
@@ -17,4 +17,83 @@ connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected as ID ${connection.threadId}`);
     // call inquirier prompt function here
-})
+    startMenu();
+});
+
+startMenu = () => {
+    inquirer.prompt(
+        {
+            type: 'list',
+            message: 'Welcome to the Employee Tracker! What would you like to do?',
+            name: 'menuChoice',
+            choices: [
+                'View ALL employees',
+                'View employees by ROLE',
+                'View employees by DEPARTMENT',
+                'ADD employee',
+                'ADD role',
+                'ADD department',
+                'UPDATE employee role'
+            ]
+        }
+    ).then((data) => {
+        switch (data.menuChoice) {
+            case 'View ALL employees':
+                viewAllEmployees();
+                break;
+            case 'View employees by ROLE':
+                viewEmployeeRoles();
+                break;
+            case 'View employees by DEPARTMENT':
+                viewEmployeeDept();
+                break;
+            case 'ADD employee':
+                addEmployee();
+                break;
+            case 'ADD role':
+                addRole();
+                break;
+            case 'ADD department':
+                addDept();
+                break;
+            case 'UPDATE employee role':
+                updateEmployeeRole();
+                break;
+        }
+    });
+};
+
+// VIEW ALL EMPLOYEES FUNCTION
+viewAllEmployees = () => {
+  console.log('view employees');
+};
+
+// VIEW EMPLOYEES BY ROLE FUNCTION 
+viewEmployeeRoles = () => {
+    console.log('view roles');
+};
+
+// VIEW EMPLOYEES BY DEPARTMENT FUNCTION
+viewEmployeeDept = () => {
+    console.log('view dept');
+};
+
+// ADD EMPLOYEES FUNCTION
+addEmployee = () => {
+    console.log('add employee');
+};
+
+// ADD ROLES FUNCTION
+addRole = () => {
+    console.log('add role');
+};
+
+// ADD DEPARTMENT FUNCTION 
+addDept = () => {
+    console.log('add dept');
+};
+
+// UPDATE EMPLOYEE ROLE FUNCTION
+updateEmployeeRole = () => {
+    console.log('update employee');
+};
