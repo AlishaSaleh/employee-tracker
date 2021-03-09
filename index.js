ad5534256522f8cfca184a3e688ae0b8de6f68b2
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-03-09 18:32:11 +0000

    Connection to database

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
+require('dotenv').config();
+
+// Connecting to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'employees_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected as ID ${connection.threadId}`);
+    // call inquirier prompt function here
+})
